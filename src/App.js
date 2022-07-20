eec2b3e84ea476cfa139b24aee8b529df95278f2
Your Name <you@example.com>
2022-07-21 00:41:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Link } from 'react-router-dom';
+import Header from './Pages/Shared/Header/Header';
+import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
+import Blogs from './Pages/Blogs/Blogs';
+import NotFound from './Pages/NotFound/NotFound';
+import Footer from './Pages/Shared/Footer/Footer';
+import Tools from './Pages/Tools/Tools';
+import Reviews from './Pages/Reviews/Reviews';
+import BusinessSummary from './Pages/BusinessSummary/BusinessSummary';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/tools' element={<Tools></Tools>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/business-summary' element={<BusinessSummary></BusinessSummary>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/BusinessSummary/BusinessSummary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BusinessSummary = () => {
+    return (
+        <div>
+            <h2>This is Business Summary Page</h2>
+        </div>
+    );
+};
+
+export default BusinessSummary;
