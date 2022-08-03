7d883b9a980b512aff669e6cde79f262181cda83
Your Name <you@example.com>
2022-08-03 17:54:48 +0600

    end

--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -5,7 +5,7 @@ const MakeAdmin = () => {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        fetch('https://calm-lake-97858.herokuapp.com/user', {
+        fetch('https://calm-lake-97858.herokuapp.com/allUser', {
             method: 'GET',
             headers: {
                 'authorization': `Bearer ${localStorage.getItem('accessToken')}`
